de1505ced4cf37d7b3ecc6fa1b5e4c2b215c8673
DrFrezze71 <pixel633@gmail.com>
2022-12-17 18:15:44 +0100

    fix

--- js.js
+++ js.js
@@ -75,20 +75,75 @@ POPUP.addEventListener("click", (e) => {
 function isInViewport(el) {
     const rect = el.getBoundingClientRect();
     console.log(rect)
-    return (
-        rect.top >= -105 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    if(el.classList.contains("offer")){
+        return (
+            rect.top >= -364 && rect.top <= el.offsetHeight - 250
+        );
+    }else if(el.classList.contains("main")){
+        return (
+            rect.top >= 150 - el.offsetHeight
+        );
+    }else 
+    if(el.classList.contains("quali")){
 
-    );
+        return (
+            rect.top <= 250 &&  rect.top >= 400-el.offsetHeight
+        );
+    }else 
+    if(el.classList.contains("gallery1")){
+        return (
+            rect.top <= 250  &&  rect.top >= 500 - el.offsetHeight
+        );
+    }else 
+    if(el.classList.contains("contact")){
+        return (
+            rect.top <= el.offsetHeight
+        );
+    }
+    
 }
 
 
+
+
+var hom_but = document.getElementById("home_button")
+var serv_but = document.getElementById("service_button")
+var quali_but = document.getElementById("quality_button")
+var gallery_but = document.getElementById("gallery_button")
+var contact_but = document.getElementById("contact_button")
 var hom = document.getElementById("home")
+var serv = document.getElementById("service")
+var quali = document.getElementById("quality")
+var galler = document.getElementById("gallery")
+var conta = document.getElementById("contact")
 function isin(){
-    console.log(isInViewport(hom))
+    tab= [hom,serv,quali,galler,conta]
+    tab2= [hom_but,serv_but,quali_but,gallery_but,contact_but]
+    
+
+    tab.forEach((element,index) =>{
+        if(isInViewport(element)){
+            let allActive = document.querySelectorAll(".is_active");
+            allActive.forEach((item)=>{
+                item.classList.remove("is_active");})
+            tab2[index].classList.add("is_active")
+
+        }
+    })
+    
+    // if(isInViewport(hom)){
+    //     hom_but.classList.add("is_active")
+    // }
+    
 
 }
 
 document.addEventListener("scroll",isin)
+
+let clickMenu = document.querySelector("#clickMenu");
+let hamburger = document.querySelector("#menu");
+let sidebar = document.querySelector(".sidebar");
+clickMenu.addEventListener("click", () => {
+	hamburger.classList.toggle("is_active");
+	sidebar.classList.toggle("is_active");
+});
